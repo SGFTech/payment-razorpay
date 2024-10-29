4c5f8d3fc9a0efd55722f2888b48dd29849f7497
Govind Diwakar <sgf@sourcegoodfood.com>
2024-10-29 05:18:30 +0000

    fix: razorpay v2

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { ModuleProvider, Modules } from "@medusajs/framework/utils"
+import {
+  RazorpayProviderService,
+} from "./services"
+
+const services = [
+  RazorpayProviderService,
+]
+
+export default ModuleProvider(Modules.PAYMENT, {
+  services,
+})
